90e3764d7ba8f2587d5deb0d7d60e27ba3287f63
aldegad <aldegad@naver.com>
2022-10-27 17:46:58 +0900

    change name

---
+++ local_modules/theme/README.md
@@ -0,0 +1,6 @@
+# @team-devmonster/react-native-theme
+This is devmonster's react-native module for make app easily. This is compatible with devmonster's react module.
+
+## Getting started
+
+`$ npm install @team-devmonster/react-native-theme`
---
+++ local_modules/theme/index.tsx
@@ -0,0 +1,24 @@
+import { createContext, useContext } from "react";
+import { ColorSchemeName, useColorScheme } from "react-native";
+
+const ThemeContext = createContext(null) as any;
+
+export type Color = {
+  light: {[name:string]:string},
+  dark: {[name:string]:string}
+}
+
+export function ThemeProvider<S extends Color,T extends Function>({children, color, theme}:{children:React.ReactNode, color:S, theme:T}) {
+
+  const colorScheme = useColorScheme() as NonNullable<ColorSchemeName>;
+
+  return (
+    <ThemeContext.Provider value={theme(color[colorScheme])}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
+
+export function useTheme<T>() {
+  return useContext<T>(ThemeContext);
+}
