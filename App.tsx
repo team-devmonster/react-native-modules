ad49f1608f8213fd1db90f82c8c23c89adbfa6c9
aldegad <aldegad@naver.com>
2022-10-27 17:47:37 +0900

    remove useTheme

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import { ThemeProvider } from '@local_modules/useTheme';
+import { ThemeProvider } from '@local_modules/theme';
 
 import Navigation from './App.navigation';
 
--- local_modules/useTheme/README.md
+++
@@ -1,2 +0,0 @@
-# @team-devmonster/react-native-theme
-This is devmonster's react-native module for make app easily. This is compatible with devmonster's react module.
